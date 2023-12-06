65c9cedf7de5941e83ecb4c3494110e76e7c8d7c
Henrik Larsson <hankisch@gmail.com>
2023-12-06 09:11:50 +0100

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css'
 
 import { IBM_Plex_Mono, Inter, PT_Serif } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const serif = PT_Serif({
   variable: '--font-serif',
@@ -30,7 +31,10 @@ export default async function RootLayout({
       lang="en"
       className={`${mono.variable} ${sans.variable} ${serif.variable}`}
     >
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
